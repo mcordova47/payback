87899bf66544ad4deb024f8abf2ff5269f40bc53
Marc Cordova <mcordova@gmail.com>
2017-11-06 20:33:23 -0500

    refactor: remove console log

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const mountNode = document.getElementById('main')
 const app = Elm.Main.embed(mountNode)
 
 app.ports.upload.subscribe(file => {
-  console.log(file)
   const fileReader = new FileReader()
   fileReader.onload = () => app.ports.readFile.send(fileReader.result)
   fileReader.readAsText(file)
